f60d91bbeca76b7036abf91e788ff1569076fee4
Shani M S <shani.ms@zarthi.com>
2026-01-03 11:26:38 +0530

    runtime error fixed

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "skilltestnextjs.evidam.zybotechlab.com",
+      },
+    ],
+  },
+};
+
 
 export default nextConfig;
